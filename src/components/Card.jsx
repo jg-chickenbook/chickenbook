3127fe9a52ffcc5e60070cfdaf1e5ea5cef635e3
Miloslav Jezek <miloslav.jezek@fpt.sk>
2023-11-12 16:01:43 +0100

    Create Chickenbook initial draft
    Add gitignore, dependencies and ESLint configuration
    
    Prepare card draft and create dummy list for testing
    
    Implement CardList component and make cards slightly bigger
    
    Implement search (name, headline, skills...) and scroll view; Improve styling

---
+++ src/components/Card.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import JGchicken from "../assets/jg-chicken.png";
+import {
+  ProfileCard,
+  ProfileImage,
+  Status,
+  Headline,
+  Name,
+  ProfileSection,
+  SectionTitle,
+  ContactInfo
+} from "./Card.styling";
+
+
+const Card = ({member}) => {
+  const skillList = member.mainSkills.map(
+    (skill) => <li key={skill}>{skill}</li>
+  );
+
+  const projectList = member.bestProjects.map(
+    (project) => <li key={project}>{project}</li>
+  );
+
+  return (
+    <ProfileCard>
+      <ProfileImage alt="Profile Image" src={JGchicken} />
+      <div>
+        <Status>{member.status}</Status>
+        <Name>{member.name}</Name>
+        <Headline>{member.headline}</Headline>
+        <details>
+          <summary>More Information</summary>
+          
+          <ProfileSection>
+            <SectionTitle>Main Skills</SectionTitle>
+            {skillList}
+          </ProfileSection>
+
+          <ProfileSection>
+            <SectionTitle>Best projects</SectionTitle>
+            {projectList}
+          </ProfileSection>
+
+          <ProfileSection>
+            <SectionTitle>Contact</SectionTitle>
+            <ContactInfo>Phone: {member.phone}</ContactInfo>
+            <ContactInfo>E-mail: {member.email}</ContactInfo>
+          </ProfileSection>
+        </details>
+      </div>
+    </ProfileCard>
+  );
+}
+
+export default Card;
---
+++ src/members.js
@@ -0,0 +1,62 @@
+export const members = [
+  {
+    id: 1,
+    status: "Employed",
+    name: "Jan Novak",
+    headline: "Java Developer",
+    mainSkills: ["Java", "Spring Boot", "PostgreSQL"],
+    bestProjects: ["Project 1", "Project 2"],
+    phone: "123 456 789",
+    email: "jan.novak@gmail.com"
+  },
+  {
+    id: 2,
+    status: "Looking for job",
+    name: "Emilie Podhorska",
+    headline: "Frontend Developer",
+    mainSkills: ["React", "Vue", "Figma"],
+    bestProjects: ["Project 1", "Project 2"],
+    phone: "123 456 789",
+    email: "emilie.podhorska@gmail.com"
+  },
+  {
+    id: 3,
+    status: "Contractor",
+    name: "Zikmund Vysoky",
+    headline: "Database Developer",
+    mainSkills: ["PostgreSQL", "MySQL"],
+    bestProjects: ["Project 1", "Project 2"],
+    phone: "123 456 789",
+    email: "zikmund.vysoky@gmail.com"
+  },
+  {
+    id: 4,
+    status: "Student",
+    name: "Eliska Jedla",
+    headline: "Python Developer",
+    mainSkills: ["Python", "Django", "PostgreSQL"],
+    bestProjects: ["Project 1", "Project 2"],
+    phone: "123 456 789",
+    email: "eliska.jedla@gmail.com"
+  },
+  {
+    id: 5,
+    status: "Learning",
+    name: "Martin Zabrana",
+    headline: "C# Developer",
+    mainSkills: ["C#", ".NET", "C++"],
+    bestProjects: ["Project 1", "Project 2"],
+    phone: "123 456 789",
+    email: "martin.zabrana@gmail.com"
+  },
+  {
+    id: 6,
+    status: "Employed",
+    name: "Jana Lorenz",
+    headline: "Android Developer",
+    mainSkills: ["Java", "Kotlin", "Jetpack Compose"],
+    bestProjects: ["Project 1", "Project 2"],
+    phone: "123 456 789",
+    email: "jana.lorenz@gmail.com"
+  }
+]
