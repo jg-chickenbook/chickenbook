6099edb8aebdd5567b2f1f0fda1b4d2eebbeb8f9
VeveCambor <camborova.veronika@gmail.com>
2024-02-08 19:17:30 +0100

    change login-form styles

--- src/pages/login/LogInForm.jsx
+++ src/pages/login/LogInForm.jsx
@@ -6,7 +6,7 @@ function LogInForm() {
   return (
     <div className="login-form">
       <form>
-        <h1>Log in</h1>
+        <h1 className="login-title">Log in</h1>
         <label htmlFor="email">Email</label>
         <input type="email" id="email" name="email" required />
         <label htmlFor="password">Password</label>
