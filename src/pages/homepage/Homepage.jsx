7d1a778d28bcb3b74189fbb86f9713cbeb577680
VeveCambor <camborova.veronika@gmail.com>
2024-03-30 16:04:23 +0100

    Delete comment parts

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,46 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import CardList from "./cards/CardList";
-// import SearchBox from "./SearchBox";
-// import { members as membersDummyList } from "../../data/members";
-// import ScrollView from "./ScrollView";
-// import "../login/LogInForm";
-// import { Link } from "react-router-dom";
-
-// export default function Homepage() {
-
-//   const [members, setMembers] = useState([]);
-//   const [searchfield, setSearchfield] = useState("");
-
-//   useEffect(() => {
-//     setMembers(membersDummyList);
-//   }, []);
-
-//   const onSearchChange = (event) => {
-//     setSearchfield(event.target.value);
-//   };
-
-//   const filteredMembers = members.filter((member) => {
-//     return member.name.toLowerCase().includes(searchfield.toLowerCase()) ||
-//       member.headline.toLowerCase().includes(searchfield.toLowerCase()) ||
-//       member.mainSkills.join(" ").toLowerCase().includes(searchfield.toLowerCase());
-//   });
-
-//   return (
-//     <>
-//       <header className="header">
-//         <h1 className="main__title">Chickenbook</h1>
-//         <SearchBox searchChange={ onSearchChange } />
-//         <Link className="nav__link" to="/login">Log In</Link>
-//       </header>
-//       <main>
-//         <ScrollView>
-//           <CardList members={ filteredMembers } />
-//         </ScrollView>
-//       </main>
-//     </>
-//   );
-// }
-
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 import CardList from "./cards/CardList";
@@ -78,14 +35,6 @@ export default function Homepage() {
       member.mainSkills.join(" ").toLowerCase().includes(searchfield.toLowerCase())
     );
   });
-
-  // const handleLogout = () => {
-  //   // Clear the user token and username from local storage
-  //   localStorage.removeItem("token");
-  //   localStorage.removeItem("username");
-  //   // Redirect to the login page or refresh the homepage
-  //   navigate("/login");
-  // };
 
   const handleLogout = async () => {
     const token = localStorage.getItem("token");
