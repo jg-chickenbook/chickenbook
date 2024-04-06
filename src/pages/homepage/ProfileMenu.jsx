212e30e29522e43d7e817e15df9ff1d7a21cac34
VeveCambor <camborova.veronika@gmail.com>
2024-04-06 13:18:26 +0200

    Add styles for ProfileMenu

--- src/pages/homepage/ProfileMenu.jsx
+++ src/pages/homepage/ProfileMenu.jsx
@@ -1,6 +1,7 @@
 // import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import "../../style/_profile-menu.scss";
 
 function ProfileMenu() {
   const navigate = useNavigate();
@@ -11,10 +12,10 @@ function ProfileMenu() {
 
   return (
     <div className="profile__menu">
-      <Link to="/edit" onClick={() => handleNavigation("/edit")}>
+      <Link className="profile-menu__link" to="/edit" onClick={() => handleNavigation("/edit")}>
         Add Profile
       </Link>
-      <Link to="/edit" onClick={() => handleNavigation("/edit")}>
+      <Link className="profile-menu__link" to="/edit" onClick={() => handleNavigation("/edit")}>
         Edit Profile
       </Link>
     </div>
