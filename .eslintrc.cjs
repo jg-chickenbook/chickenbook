5526dea7f7ebad33d438c700499fb73f7643f6a5
Miloslav Jezek <milo.jezek@gmail.com>
2024-04-20 16:54:26 +0200

    Add and apply new ESLint rules with ESLint Stylistic

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,29 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "@stylistic", "react", "react-hooks"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "@stylistic/quotes": ["error", "double"],
+    "@stylistic/jsx-quotes": ["error", "prefer-double"],
+    "@stylistic/no-multiple-empty-lines": ["error", { max: 2, maxEOF: 0, maxBOF: 0}],
+    "@stylistic/array-bracket-spacing": ["error", "never"],
+    "@stylistic/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+    "@stylistic/indent": ["error", 2, { SwitchCase: 1 }],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-vars": "error",
+    "react/jsx-uses-react": "error",
+    "allowImportExportEverywhere": 0,
+    "react/prop-types": "off",
+    "@stylistic/semi": ["error", "always"]
   },
 }
