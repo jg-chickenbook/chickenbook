eb20a471aef3c7053a62a30ae19d52e73e33fa0d
Miloslav Jezek <milo.jezek@gmail.com>
2024-04-29 20:57:17 +0200

    Update the useFetchData to meet TS and ESLint requirements; Add error handling

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,19 +1,33 @@
 import { useState, useEffect } from "react";
 
-const useFetchData = (url: string) => {
-  const [data, setData] = useState<any>(null);
+type FetchReturnType<T> = {
+  data: T | null
+  loading: boolean
+  error: string | null
+};
+
+const useFetchData = <T>(url: string): FetchReturnType<T> => {
+  const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
+    const fetchData = async (): Promise<void> => {
+      try {
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (e) {
+        setError("Error fetching data");
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+
+    fetchData();
   }, [url]);
 
-  return { data, loading };
+  return { data, loading, error };
 };
 
 export default useFetchData;
